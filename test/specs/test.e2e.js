0b1fe013a78b5967d5e3e4128f3362e416f068ed
OlgaRomankova <olcha777@tut.by>
2024-05-09 23:48:23 +0300

    cloud storage page

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,6 +1,7 @@
 // import { expect } from '@wdio/globals'
 import addToEstimatePage from "../pageobjects/addToEstimatePage.js";
 import calculatorPage from "../pageobjects/calculatorPage.js";
+import cloudStoragePage from "../pageobjects/cloudStoragePage.js";
 import welcomePage from "../pageobjects/welcomePage.js";
 import actions from "./../actions.js";
 
@@ -28,5 +29,29 @@ describe("Open calculator", () => {
   });
   it("should click on plus two button", async () => {
     await actions.clickElement(calculatorPage.plusTwoButton);
+  });
+  it("should click on minus three button", async () => {
+    await actions.clickElement(calculatorPage.minusThreeButton);
+  });
+  it("should click on plus three button", async () => {
+    await actions.clickElement(calculatorPage.plusThreeButton);
+  });
+  it("should click on spot button", async () => {
+    await actions.clickElement(calculatorPage.spotButton);
+  });
+  it("should click on regular button", async () => {
+    await actions.clickElement(calculatorPage.regularButton);
+  });
+  it("should click on balancedPersistentDisk button", async () => {
+    await actions.clickElement(calculatorPage.balancedPersistentDiskButton);
+  });
+  it("should click on three year button", async () => {
+    await actions.clickElement(calculatorPage.threeyearsButton);
+  });
+  it("should click on add to estimate 1", async () => {
+    await actions.clickElement(calculatorPage.addToEstimateButton1);
+  });
+  it("should click on minus one button", async () => {
+    await actions.clickElement(cloudStoragePage.cloudStorageButton);
   });
 });
