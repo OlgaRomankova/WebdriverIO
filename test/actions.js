8adaf8f8d7288d5d5d2e68e8328c9eb3706bf0d5
OlgaRomankova <olcha777@tut.by>
2024-05-14 21:58:14 +0300

    Everything is stable

--- test/actions.js
+++ test/actions.js
@@ -1,14 +1,17 @@
-import { $ } from '@wdio/globals'
+import { $ } from "@wdio/globals";
 
 class actions {
+  async open(path) {
+    await browser.url(
+      `https://cloud.google.com/products/calculator?hl=ru/${path}`
+    );
+  }
 
-   async open (path) {
-      await browser.url(`https://cloud.google.com/products/calculator?hl=ru/${path}`)
-    }
-
-    async clickElement (selector) {                           
-    const element = await $(selector)
-    await element.click()
-}}
+  async clickElement(selector) {
+    const element = await $(selector);
+    await element.waitForExist({ timeout: 5000 });
+    await element.click();
+  }
+}
 
 export default new actions();
