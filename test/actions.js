b243549475c0bd9832698dbc57a61ff85a097e7c
Kristina <tinka_e@mail.ru>
2024-05-14 22:29:11 +0300

    update the function and test

--- test/actions.js
+++ test/actions.js
@@ -1,16 +1,20 @@
-import { $ } from "@wdio/globals";
+import { $ } from '@wdio/globals';
 
 class actions {
   async open(path) {
-    await browser.url(
-      `https://cloud.google.com/products/calculator?hl=ru/${path}`
-    );
+    await browser.url(`https://cloud.google.com/products/calculator?hl=ru/${path}`);
   }
 
   async clickElement(selector) {
     const element = await $(selector);
-    await element.waitForExist({ timeout: 5000 });
-    await element.click();
+    const elementIsDisplayed = await element.waitForDisplayed({ timeout: 12000 });
+
+    if (elementIsDisplayed) {
+      await element.moveTo();
+      await element.click();
+    } else {
+      console.log('the element is not Displayed');
+    }
   }
 }
 
