5a6dd04e15e138df6b022c4514807c5e510125d0
OlgaRomankova <olcha777@tut.by>
2024-05-19 01:39:38 +0300

    Allure uod

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,34 +1,41 @@
 export const config = {
-        runner: 'local',
-        specs: [
-        './test/specs/**/*.js'
+  reporters: [
+    [
+      "allure",
+      {
+        outputDir: "allure-results",
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: false,
+      },
     ],
-        exclude: [
-         ],
-    
-    maxInstances: 10,
-    
-    capabilities: [{
-        browserName: 'chrome'
-    }],
+  ],
 
-    
-    logLevel: 'warn',
-    
-    bail: 0,
-    
-    waitforTimeout: 10000,
-        connectionRetryTimeout: 120000,
-        connectionRetryCount: 3,
-   
-    services: ['visual'],
-     framework: 'mocha',
-        reporters: ['spec'],
- 
-    mochaOpts: {
-        ui: 'bdd',
-        timeout: 60000
+  runner: "local",
+  specs: ["./test/specs/**/*.js"],
+  exclude: [],
+
+  maxInstances: 10,
+
+  capabilities: [
+    {
+      browserName: "chrome",
     },
+  ],
 
-   
-}
+  logLevel: "warn",
+
+  bail: 0,
+
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+
+  services: ["visual"],
+  framework: "mocha",
+  reporters: ["spec"],
+
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 60000,
+  },
+};
